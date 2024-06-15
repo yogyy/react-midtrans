2c7def24aacb4a7febc3f35742406b3e8701fbc6
yogyy <yogimr261@gmail.com>
2024-06-15 22:04:26 +0700

    chor: update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import { Header } from "./components/header";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home";
+import Error from "./pages/error";
+import Checkout from "./pages/checkout";
+import OrderStatus from "./pages/order-status";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/checkout",
+    element: <Checkout />,
+  },
+  {
+    path: "/order-status",
+    element: <OrderStatus />,
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <Header title="Toko Moretti" />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
